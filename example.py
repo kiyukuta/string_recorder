6435655c8b14afb5e750598bd7f4eb84a2ee8425
Yuta Kikuchi <yut.kkc@gmail.com>
2017-02-23 12:54:51 +0900

    add example.py

---
+++ example.py
@@ -0,0 +1,7 @@
+import string_recorder
+import random
+rec = string_recorder.StringRecorder()
+for i in range(10):
+    x = random.randint(0, 5)
+    rec.record_frame('{}{}\n{}'.format(i, '>' *x, 'v\n' * x))
+rec.make_gif('test.gif')
