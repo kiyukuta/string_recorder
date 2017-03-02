f9e922ee19941fdb061374a8ef71dfc4525582df
Yuta Kikuchi <yut.kkc@gmail.com>
2017-03-02 23:03:46 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,12 @@ from setuptools import setup
 
 setup(
     name='string_recorder',
-    version='0.0.1'
-    )
+    version='0.0.1',
+    author='Yuta Kikuchi',
+    author_email='kikuchi@preferred.jp',
+    install_requires=[
+        'numpy',
+        'pillow',
+        'imageio',
+    ]
+)
