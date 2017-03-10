1e4028cb5fa43c84e553e06a7a5b50151c5ce57d
Tetsuo Kiso <tetsuokiso9@gmail.com>
2017-03-11 05:51:47 +0900

    Use curl on macOS
    On macOS, `wget` is not available by default. `curl` is installed
    instead.
    
    Also, enable the `strict mode`.

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,2 +1,11 @@
-wget https://github.com/adobe-fonts/source-han-code-jp/archive/2.000R.tar.gz
+#!/bin/bash
+set -o errexit
+set -o nounset
+
+CMD=wget
+# On macOS, wget is not available by default.
+if [[ `uname -s` = "Darwin" ]]; then
+  CMD="curl -L -O"
+fi
+$CMD https://github.com/adobe-fonts/source-han-code-jp/archive/2.000R.tar.gz
 tar zxvf 2.000R.tar.gz
